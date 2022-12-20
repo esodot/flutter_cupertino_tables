89ea40661552b972d9d8e5be5d455e54a2bf5fbe
Alexander Gerk <Alexander.Gerk@zeppelin.com>
2022-12-20 13:05:53 +0100

    Added widget test

--- test/cupertino_tables_test.dart
+++ test/cupertino_tables_test.dart
@@ -1,11 +1,53 @@
+import 'package:cupertino_tables/cupertino_table_row.dart';
+import 'package:cupertino_tables/cupertino_table_section.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test("cupertino_table_section", () {
-      expect("", "");
-  });
+  // Define a test.
+  testWidgets('widget test', (tester) async {
+    var pressed = false;
+    onPressed() => pressed = true;
 
-  test("cupertino_table_row", () {
-    expect("", "");
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(CupertinoApp(
+        home: CupertinoTableSection(
+      header: const Text("header"),
+      footer: const Text("footer"),
+      children: [
+        CupertinoTableRow(
+          title: const Text("title"),
+          leading: const CupertinoTableRowIcon(
+            icon: CupertinoIcons.settings,
+          ),
+          helper: const Text("helper"),
+          trailing: const CupertinoTableRowNavigationIndicator(),
+          onPress: onPressed,
+        ),
+      ],
+    )));
+
+    // Create the Finders.
+    final headerFinder = find.text('header');
+    final footerFinder = find.text('footer');
+    final titleFinder = find.text('title');
+    final helperFinder = find.text("helper");
+    final iconFinder =
+        find.widgetWithIcon(CupertinoTableRowIcon, CupertinoIcons.settings);
+    final indicatorFinder = find.widgetWithIcon(
+        CupertinoTableRowNavigationIndicator, CupertinoIcons.forward);
+
+    // Use the `findsOneWidget` matcher provided by flutter_test to verify
+    // that the widgets appear exactly once in the widget tree.
+    expect(headerFinder, findsOneWidget);
+    expect(footerFinder, findsOneWidget);
+    expect(titleFinder, findsOneWidget);
+    expect(helperFinder, findsOneWidget);
+    expect(iconFinder, findsOneWidget);
+    expect(indicatorFinder, findsOneWidget);
+
+    // We are testing the tap
+    await tester.tap(titleFinder);
+    expect(pressed, isTrue);
   });
 }
